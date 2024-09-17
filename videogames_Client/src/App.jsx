c6c6c5d8ea4e6a2e0bc45fe88a859bba51814a7d
Maria Delfina Samperi <delfisamperi@gmail.com>
2024-09-17 00:54:56 -0300

    first commit for the client side of the app

---
+++ videogames_Client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Is this the Landing Page of Videogames App?</h1>
+      <h2>Looks like it is indeed...</h2>
+      
+    </>
+  )
+}
+
+export default App
