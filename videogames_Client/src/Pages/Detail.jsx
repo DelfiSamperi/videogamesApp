4c9f98c1cf9ce4efd419d70a4ffcc462a05eae60
Maria Delfina Samperi <delfisamperi@gmail.com>
2024-10-01 15:08:01 -0300

    detail

--- videogames_Client/src/Pages/Detail.jsx
+++ videogames_Client/src/Pages/Detail.jsx
@@ -13,17 +13,17 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import './detail.css';
 
-const Detail = () => {
+const Detail = ({name, releaseDate, rating}) => {
     return (
         <div>
             <h3>This is Detail Page</h3>
             <div className="detail-card">
                 <h5>Videogame ID</h5>
-                <h5>Videogame Name</h5>
+                <h5>{name}</h5>
                 <h5>Videogame Platforms</h5>
                 <h5>Videogame Description</h5>
-                <h5>Videogame Release Date</h5>
-                <h5>Videogame Rating</h5>
+                <h5>{releaseDate}</h5>
+                <h5>{rating}</h5>
                 <h5>Videogame Genres</h5>
             </div>
             <Link to='/home'>
